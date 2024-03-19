5700b010c256a8989b84a307909d052b7d17a758
vojhab <voj.hab@proton.me>
2024-03-19 11:29:57 +0100

    Add login function

--- src/user.js
+++ src/user.js
@@ -78,10 +78,71 @@ const createUser = async (
       console.log(`User with email ${email} created in db`);
     }
   } catch (error) {
+    if (error.code === 11000) {
+      throw {
+        code: "auth/duplicate-email",
+        message: "User with this email already exists",
+        status: 409,
+      };
+    } else {
+      throw {
+        code: "auth/create-user-error",
+        message: "Failed to create user",
+        status: 500,
+      };
+    }
+  }
+
+  const accessToken = await generateAccessToken(auth, userDoc._id);
+  const refreshToken = await generateRefreshToken(auth, userDoc._id);
+
+  return {
+    user: userDoc,
+    accessToken,
+    refreshToken,
+  };
+};
+
+const login = async (auth, { email, password }) => {
+  if (auth.settings.enableLogs) {
+    console.log(`Logging in user with email ${email}`);
+  }
+
+  if (!email || !password) {
     throw {
-      code: "auth/create-user-error",
-      message: "Failed to create user",
-      status: 500,
+      code: "auth/missing-credentials",
+      message: "Email and password are required",
+      status: 400,
+    };
+  }
+
+  const emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  if (!emailRegex.test(email)) {
+    throw {
+      code: "auth/invalid-email",
+      message: "The email address is badly formatted",
+      status: 400,
+    };
+  }
+
+  const userDoc = await auth.models.User.findOne({ email });
+
+  if (!userDoc) {
+    throw {
+      code: "auth/user-not-found",
+      message: "User with this email does not exist",
+      status: 404,
+    };
+  }
+
+  const passwordMatch = await bcrypt.compare(password, userDoc.passwordHash);
+
+  if (!passwordMatch) {
+    throw {
+      code: "auth/wrong-password",
+      message: "The password is invalid",
+      status: 401,
     };
   }
 

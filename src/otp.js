9bb06b334965eb9fcd81ce96a6393140a22d49e1
Xenom-Apperance <xenomcz27@gmail.com>
2024-03-26 13:44:28 +0100

    Finish the validateTOTP and generateTOTP functions, add the checking of auth object

--- src/otp.js
+++ src/otp.js
@@ -2,41 +2,88 @@ const crypto = require("node:crypto");
 const OTPAuth = require("otpauth");
 const base32 = require("hi-base32");
 const qrcode = require("qrcode");
+const { type } = require("node:os");
 
-const generateBase32Secret = function () {
-  const buffer = crypto.randomBytes(128);
-  const secret = base32.encode(buffer).replace(/=/g, "").substring(0, 32);
+const generateBase32Secret = function (auth) {
+  if (auth.settings.enableLogs) {
+    console.info(
+      `Generating a base32 secret for TOTP of length ${auth.settings.twofa.keylength}`
+    );
+  }
+  const buffer = crypto.randomBytes(auth.settings.twofa.keylength);
+  const secret = base32.encode(buffer).replace(/=/g, "");
+  console.info("Generated TOTP key successfuly");
   return secret;
 };
 
-const generateTOTP = function () {
-  const secret = generateBase32Secret();
+const generateTOTP = function (auth, label) {
+  if (
+    typeof auth.settings.twofa == "undefined" ||
+    typeof label == "undefined"
+  ) {
+    console.error(
+      "InfinityUser TOTP Error: twoFA object or label was not defined."
+    );
+    return;
+  }
+  const secret = generateBase32Secret(auth);
+  if (auth.settings.enableLogs) {
+    console.info("Generating TOTP auth object");
+  }
+
   const totp = new OTPAuth.TOTP({
-    issuer: "example.com",
-    label: "example",
+    issuer: auth.settings.twofa.issuer,
+    label: auth.settings.twofa.label,
     algorithm: "SHA256",
     digits: 6,
     secret: secret,
   });
-  qrcode.toFile("./test.png", totp.toString());
   return totp.toString();
 };
 
-const validateTOTP = function (code, secret) {
+const validateTOTP = function (auth, code, secret) {
+  if (
+    typeof auth.settings.twofa == "undefined" ||
+    typeof code == "undefined" ||
+    typeof secret == "undefined"
+  ) {
+    console.error(
+      "InfinityUser TOTP Error: twoFA object, label or secret were undefined during validation."
+    );
+    return;
+  }
+  if (auth.settings.enableLogs) {
+    console.info(`Validating TOTP code ${code}`);
+  }
   const totp = new OTPAuth.TOTP({
-    issuer: "example.com",
-    label: "example",
     algorithm: "SHA256",
     digits: 6,
     secret: secret,
   });
-  const delta = totp.validate({ token: code, window: 1 });
+  let delta;
+  if (typeof auth.settings.twofa.delta == "undefined") {
+    delta = totp.validate({ token: code, window: 1 });
+  } else {
+    delta = totp.validate({ token: code, window: auth.settings.twofa.delta });
+  }
   if (delta == null) {
+    if (auth.settings.enableLogs) {
+      console.log(`TOTP code ${code} was not found in the specified window.`);
+    }
     return false;
   } else {
+    if (auth.settings.enableLogs) {
+      console.log(`TOTP code ${code} was found in the specified window.`);
+    }
     return true;
   }
 };
 
 // console.log(generateTOTP());
-// console.log(validateTOTP("602410", "7AOTJVQTKCFIWV27J5DTM3WEPXG4AB72"));
+// console.log(
+//   validateTOTP(
+//     { settings: { enableLogs: true } },
+//     "602410",
+//     "7AOTJVQTKCFIWV27J5DTM3WEPXG4AB72"
+//   )
+// );

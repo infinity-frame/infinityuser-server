452e88ea31cb192f3587c5c60e558196c0b04047
vojhab <voj.hab@proton.me>
2024-04-29 09:56:43 +0200

    Optimization

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,12 +4,6 @@ const { isPasswordCorrect } = require("../user");
 const authMiddleware = (auth) => {
   return async (req, res, next) => {
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      res
-        .status(401)
-        .json({ code: "auth/missing-header", message: "Missing auth header." });
-      return;
-    }
     const accessToken = authHeader && authHeader.split(" ")[1];
     if (!accessToken) {
       res.status(401).json({
